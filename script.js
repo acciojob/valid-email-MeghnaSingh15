de52b8dfbaa7054375309c4c8ae24a897bc2145a
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-11 12:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function validEmail(str) {
   //your JS code here.
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	const isValidEmail = emailRegex.test(str);
+	return isValidEmail;
 }
 
 // Do not change the code below.
